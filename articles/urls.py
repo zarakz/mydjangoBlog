0e3e2d77b67b3b90223c99f5d9ed06f05000c072
zarakz <z.kermanizadeh@gmail.com>
2023-12-18 22:20:34 +0330

    urls

--- articles/urls.py
+++ articles/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+
+app_name = "articles"
 urlpatterns = [
-    path('', views.articles_list),
+    path('', views.articles_list, name = "list"),
+    path('<slug>', views.article_detail, name = "detail")
 ]
 
 urlpatterns += staticfiles_urlpatterns()
