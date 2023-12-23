b7e7706dc84eb3f22671cfc8e6e92c429962befe
zarakz <z.kermanizadeh@gmail.com>
2023-12-23 18:21:59 +0330

    create dynamic image for each article

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 app_name = "articles"
@@ -9,4 +11,3 @@ urlpatterns = [
     path('<slug>', views.article_detail, name = "detail")
 ]
 
-urlpatterns += staticfiles_urlpatterns()
--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -122,6 +122,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
